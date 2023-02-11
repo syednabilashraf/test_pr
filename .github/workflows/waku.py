f90a2f6b11c63df4c586063ef3e4e42f612193ce
syednabilashraf <61734391+syednabilashraf@users.noreply.github.com>
2023-02-11 20:47:42 +0600

    Update waku.py

--- .github/workflows/waku.py
+++ .github/workflows/waku.py
@@ -1,4 +1,8 @@
 import os
+import json
 
 if __name__ == '__main__':
   print("variables", os.environ)
+  with open(os.environ.get("GITHUB_EVENT_PATH", 'r') as event_file:
+            event = json.load(event_file)
+            print("got event", event)
